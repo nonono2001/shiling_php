916c626b43251b61573abbb7cc8f2d4529db3d56
zhangtinghua <zhangtinghua@shopex.cn>
2017-01-23 10:41:20 +0800

    test

--- module/login.mod.php
+++ module/login.mod.php
@@ -89,7 +89,9 @@ class ModuleObject extends MasterObject
 		$code = $_GET['code'];
 		$session = $this->get_key($code);
 		$session_array = print_r($session,true);
+		print_r($session_array,true);
 		$pc = new WXBizDataCrypt($this->appid, $session_array['session_key']);
+		print_r($pc,true);
 		echo json_encode($pc);
 	}
 	
